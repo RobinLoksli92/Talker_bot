5b7e7907d3eabe7ce3fbb15dd985b94d8b47fbb4
RobinLoksli92 <gleb11tiun@mail.ru>
2022-06-27 14:01:05 +0200

    Setup DialogFlow

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
+import requests
 from telegram import Update
 from telegram.ext import Filters, Updater
 from telegram.ext import CommandHandler, CallbackContext, MessageHandler
 
+from google.cloud import dialogflow
+
 
 states_db = {}
+project_id = 'sunlit-ace-354318'
+
 
 
 def start(update: Update, context: CallbackContext):
@@ -16,10 +21,26 @@ def start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
+    chat_id = update.message.chat_id
+    text = detect_intents_text(project_id, session_id=chat_id, texts=[update.message.text])
     update.message.reply_text(
-        text=update.message.text
+        text=text
     )
-    return
+    return 'ECHO'
+
+
+def detect_intents_text(project_id, session_id, texts, language_code='RU-ru'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+    return response.query_result.fulfillment_text
 
 
 def handle_user_reply(update: Update, context: CallbackContext):
@@ -32,10 +53,6 @@ def handle_user_reply(update: Update, context: CallbackContext):
     
     if user_reply == '/start':
         user_state = 'START'
-    elif user_reply != '/start':
-        update.message.reply_text(
-            text='Для начала работы с ботом, напишите /start'
-        )
     else:
         user_state = states_db.get(chat_id)
     
@@ -52,6 +69,7 @@ def handle_user_reply(update: Update, context: CallbackContext):
 def main():
     load_dotenv()
     telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    json_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
     updater = Updater(telegram_bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', handle_user_reply))
